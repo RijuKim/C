badf545cd562d0579c9d944ad85c9939acf20e8e
RijuKim <rijoo1121@gmail.com>
2022-03-05 15:16:38 +0900

    Add: 고양이 게임 완성

--- C 프로그래밍-입문부터 게임개발까지/고양이_수집하기_게임/고양이_수집하기_게임/struct.c
+++ C 프로그래밍-입문부터 게임개발까지/고양이_수집하기_게임/고양이_수집하기_게임/struct.c
@@ -35,13 +35,15 @@ int main (void){
 		printf("두근두근~ 어느 고양이의 집사가 될까요?\n아무 키나 눌러서 확인하세요!");
 		getchar();
 
-		int selected = rand() * 5; //0-4 사이의 숫자 반환
+		int selected = rand() % 5; //0-4 사이의 숫자 반환
 		printCat(selected); //뽑은 고양이 정보 출력
 		collection[selected] = 1; //고양이 뽑기 처리
 
 		int collectAll = checkCollection();
+		if (collectAll==1) {
+			break;
+		}
 	}
-	return 0;
 }
 
 void initCats() {
@@ -51,25 +53,25 @@ void initCats() {
 	cats[0].character = "온순";
 	cats[0].level = 1;
 
-	cats[0].name = "구름이";
-	cats[0].age = 11;
-	cats[0].character = "까칠";
-	cats[0].level = 3;
+	cats[1].name = "구름이";
+	cats[1].age = 11;
+	cats[1].character = "까칠";
+	cats[1].level = 3;
 
-	cats[0].name = "꼬맹이";
-	cats[0].age = 1;
-	cats[0].character = "애교많음";
-	cats[0].level = 2;
+	cats[2].name = "꼬맹이";
+	cats[2].age = 1;
+	cats[2].character = "애교많음";
+	cats[2].level = 2;
 
-	cats[0].name = "포포";
-	cats[0].age = 6;
-	cats[0].character = "더러움";
-	cats[0].level = 5;
+	cats[3].name = "포포";
+	cats[3].age = 6;
+	cats[3].character = "더러움";
+	cats[3].level = 5;
 
-	cats[0].name = "꿀꿀이";
-	cats[0].age = 3;
-	cats[0].character = "먹보";
-	cats[0].level = 4;
+	cats[4].name = "꿀꿀이";
+	cats[4].age = 3;
+	cats[4].character = "먹보";
+	cats[4].level = 4;
 }
 
 void printCat(int selected) {
@@ -84,3 +86,30 @@ void printCat(int selected) {
 	}
 	printf("\n\n");
 }
+
+int checkCollection() {
+	//1. 현재 보유한 고양이 목록도 출력
+	//2. 다 모았는지 여부를 반환
+	int collectAll = 1;
+
+	printf("\n\n===보유한 고양이 목록 반환===\n\n");
+	for (int i = 0; i < 5; i++) {
+		if (collection[i] == 0) { //고양이 수집 x
+			printf("%10s", "(빈 박스)\n");
+			collectAll = 0; //다 모으지 못한 상태
+		}
+		else //고양이 수집 0
+		{
+			printf("%10s", cats[i].name);
+		}
+	}
+	printf("\n=======================\n\n");
+
+	if (collectAll) { //모든 고양이를 다 모은 경우
+		printf("\n\n축하합니다! 모든 고양이를 다 모았어요. 열심히 키워주세요!\n\n ");
+	}
+	return 0;
+
+	return collectAll;
+}
+
